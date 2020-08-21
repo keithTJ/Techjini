4268d405c4d2f40b66c24700e1c271f9d2f9ce32
Keith Menezes <keith.m@techjini.com>
2020-08-21 22:45:26 +0530

    styled components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import Radium from 'radium';
-import './Person.css';
+import styled from 'styled-components';
+// import './Person.css';
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin : 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    '@media (min-width: 500px)': {
+        width: '450px'
+    }
+`;
 
 const person = (props) => {
     const style = {
@@ -9,12 +22,13 @@ const person = (props) => {
         }
     };
     return (
-        <div className="Person" style={style}>
+        // <div className="Person" style={style}>
+        <StyledDiv>
             <h1 onClick={props.click}>I am {props.name} and I am {props.age} years old.</h1>
             <h2> {props.children} </h2>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        </StyledDiv>
     )
 };
 
-export default Radium(person);
+export default person;
